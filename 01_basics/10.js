cf1115063f1a9d6bd466fdf6ad974ffcd912ad4e
Nisha Yadav <nishayadav1703@gmail.com>
2025-12-30 11:16:23 +0530

    array-string question

--- 01_basics/10.js
+++ 01_basics/10.js
@@ -5,19 +5,42 @@
 // Returns an object showing how many times each string appears
 
 function countFrequencies(fruits) {
-  let count = 0;
   let obj = {};
   for (let char of fruits) {
-    // if (obj.hasOwnProperty(char)) {
-    obj[char] = count++;
-    console.log(obj);
-
-    // }
-    // count++;
+    obj[char] = (obj[char] || 0) + 1;
   }
-  console.log(obj);
+  return obj;
 }
 console.log(
   countFrequencies(["apple", "banana", "apple", "orange", "banana", "apple"])
 );
 // { apple: 3, banana: 2, orange: 1 }
+
+// result[word] = (result[word] || 0) + 1;
+// What it represents
+// If result[word] already exists → increase it by 1
+// If it does NOT exist → treat it as 0, then set it to 1
+
+// || checks falsy values
+
+// Step-by-step
+
+//1. result[word]
+// undefined for first occurrence
+// a number for later occurrences
+
+//2. (result[word] || 0)
+// If result[word] is truthy (e.g., 2) → use it
+// If undefined → fallback to 0
+
+//3. + 1
+// Increment count
+
+const result = {};
+const word = "apple";
+
+result[word] = (result[word] || 0) + 1;
+// undefined || 0 → 0 + 1 → 1
+
+result[word] = (result[word] || 0) + 1;
+// 1 || 0 → 1 + 1 → 2
